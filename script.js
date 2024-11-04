85937e1f3c35a59af27e3089f623e6465d479a2e
Wiktoria Peretiatkowicz <127852624+wkktoria@users.noreply.github.com>
2024-11-04 17:22:57 +0100

    Set up hover effect

--- script.js
+++ script.js
@@ -5,5 +5,23 @@ const GRID_SIZE = 16 * 16;
 for (let i = 0; i < GRID_SIZE; i++) {
   let square = document.createElement("div");
   square.classList.add("square");
+  square.addEventListener("mouseenter", hoverEffect);
   grid.appendChild(square);
+}
+
+function hoverEffect(event) {
+  event.target.style.backgroundColor = generateRadomColor();
+}
+
+function getRandomNumber(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+function generateRadomColor() {
+  const red = getRandomNumber(0, 255);
+  const green = getRandomNumber(0, 255);
+  const blue = getRandomNumber(0, 255);
+  const alpha = Math.random() * 1;
+
+  return `rgba(${red}, ${green}, ${blue}, ${alpha})`;
 }
